880598cc99ce7099e9c1745b7a2f858cc02a668a
Jerum Hubbert <hubbertj@gmail.com>
2018-07-28 12:42:44 -0700

    adding checkout health unit test

---
+++ tasc_exercise/test/api/controllers/checkout.controller.js
@@ -0,0 +1,42 @@
+asdasvar should = require('should');
+var request = require('supertest');
+var server = require('../../../app');
+
+describe('controllers', function() {
+    describe('health', function() {
+        describe('GET /health', function() {
+            it('should return a default status', function(done) {
+
+                request(server)
+                    .get('/health')
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err, res) {
+                        should.not.exist(err);
+
+                        res.body.should.eql('System health is good, status = UNKNOWN');
+
+                        done();
+                    });
+            });
+
+            it('should accept a status parameter', function(done) {
+
+                request(server)
+                    .get('/health')
+                    .query({ status: 'test' })
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err, res) {
+                        should.not.exist(err);
+
+                        res.body.should.eql('System health is good, status = test');
+
+                        done();
+                    });
+            });
+        });
+    });
+});
