6d732d9a2aa27b461686dae63296b9c5a4229f2b
Jerum Hubbert <hubbertj@gmail.com>
2018-07-31 12:51:34 -0700

    adding unit test for items, mod, save, delete

--- tasc_exercise/test/api/controllers/item.controller.js
+++ tasc_exercise/test/api/controllers/item.controller.js
@@ -4,6 +4,14 @@ var server = require('../../../app');
 const swaggerPath = 'api/swagger/swagger.yaml';
 const readYaml = require('read-yaml');
 
+var stubItem = {
+    "name": "testItem",
+    "import": false,
+    "except": true,
+    "type": "",
+    "price": 99.99,
+};
+
 new Promise(function(aResolve, aReject) {
     readYaml(swaggerPath, function(err, data) {
         if (err) return aReject(err);
@@ -12,47 +20,122 @@ new Promise(function(aResolve, aReject) {
 }).then((swaggerApiSettings) => {
     describe('controllers', function() {
         describe('item', function() {
-            describe('GET /health', function() {
-                it('should return a item from a itemID', function(done) {
+            describe('GET /item/{itemId}', function() {
+                it('should return a item from a itemId', function(done) {
                     request(server)
-                        .get(swaggerApiSettings.basePath + '/health')
+                        .get(swaggerApiSettings.basePath + '/item/' + 1)
                         .set('Accept', 'application/json')
                         .expect('Content-Type', /json/)
                         .expect(200)
                         .end(function(err, res) {
                             should.not.exist(err);
 
-                            res.body.should.eql('System health is good, status = UNKNOWN');
+                            res.body.should.have.property('id').which.is.a.Number();
+                            res.body.should.have.property('name').which.is.a.String();
+                            res.body.should.have.property('import').which.is.a.Boolean();
+                            res.body.should.have.property('except').which.is.a.Boolean();
+                            res.body.should.have.property('type').which.is.a.String();
+                            res.body.should.have.property('price').which.is.a.Number();
                             done();
                         });
                 });
-                it('should be able to modify a item', function(done) {
-                    request(server)
-                        .get(swaggerApiSettings.basePath + '/health')
-                        .query({ status: 'test' })
-                        .set('Accept', 'application/json')
-                        .expect('Content-Type', /json/)
-                        .expect(200)
-                        .end(function(err, res) {
-                            should.not.exist(err);
+                it('should be able to (add / delete) a item.', function(done) {
+                    new Promise((resolve, reject) => {
+                            request(server)
+                                .post(swaggerApiSettings.basePath + '/item')
+                                .send(stubItem)
+                                .set('Accept', 'application/json')
+                                .expect('Content-Type', /json/)
+                                .expect(200)
+                                .end(function(err, res) {
+                                    should.not.exist(err);
 
-                            res.body.should.eql('System health is good, status = test');
-                            done();
-                        });
+                                    res.body.should.have.property('id').which.is.a.Number();
+                                    res.body.should.have.property('name').which.is.a.String();
+                                    res.body.should.have.property('import').which.is.a.Boolean();
+                                    res.body.should.have.property('except').which.is.a.Boolean();
+                                    res.body.should.have.property('type').which.is.a.String();
+                                    res.body.should.have.property('price').which.is.a.Number();
+                                    return resolve(res.body);
+                                });
+
+                        })
+                        .then((item) => {
+                            request(server)
+                                .delete(swaggerApiSettings.basePath + '/item/' + item.id)
+                                .set('Accept', 'application/json')
+                                .expect('Content-Type', /json/)
+                                .expect(200)
+                                .end(function(err, res) {
+                                    should.not.exist(err);
+                                    done();
+                                });
+                        })
+                        .catch(err => console.error(err));
+
                 });
-                it('should delete a item from the system', function(done) {
-                    request(server)
-                        .get(swaggerApiSettings.basePath + '/health')
-                        .query({ status: 'test' })
-                        .set('Accept', 'application/json')
-                        .expect('Content-Type', /json/)
-                        .expect(200)
-                        .end(function(err, res) {
-                            should.not.exist(err);
+                it('should be able to (add / modify / delete) a item', function(done) {
+                    var item = null;
+                    new Promise((resolve, reject) => {
+                            request(server)
+                                .post(swaggerApiSettings.basePath + '/item')
+                                .send(stubItem)
+                                .set('Accept', 'application/json')
+                                .expect('Content-Type', /json/)
+                                .expect(200)
+                                .end(function(err, res) {
+                                    should.not.exist(err);
 
-                            res.body.should.eql('System health is good, status = test');
-                            done();
-                        });
+                                    res.body.should.have.property('id').which.is.a.Number();
+                                    res.body.should.have.property('name').which.is.a.String();
+                                    res.body.should.have.property('import').which.is.a.Boolean();
+                                    res.body.should.have.property('except').which.is.a.Boolean();
+                                    res.body.should.have.property('type').which.is.a.String();
+                                    res.body.should.have.property('price').which.is.a.Number();
+
+                                    res.body.price.should.eql(0.00);
+                                    return resolve(res.body);
+                                });
+
+                        })
+                        .then((newItem) => {
+                            if (newItem && 'price' in newItem) {
+                                newItem.price = 1.99;
+                            }
+                            return new Promise((resolve, reject) => {
+                                request(server)
+                                    .put(swaggerApiSettings.basePath + '/item')
+                                    .send(newItem)
+                                    .set('Accept', 'application/json')
+                                    .expect('Content-Type', /json/)
+                                    .expect(200)
+                                    .end(function(err, res) {
+                                        should.not.exist(err);
+
+                                        res.body.should.have.property('id').which.is.a.Number();
+                                        res.body.should.have.property('name').which.is.a.String();
+                                        res.body.should.have.property('import').which.is.a.Boolean();
+                                        res.body.should.have.property('except').which.is.a.Boolean();
+                                        res.body.should.have.property('type').which.is.a.String();
+                                        res.body.should.have.property('price').which.is.a.Number();
+
+                                        res.body.price.should.eql(1.99);
+                                        return resolve(res.body);
+                                    });
+
+                            });
+                        })
+                        .then((modItem) => {
+                            request(server)
+                                .delete(swaggerApiSettings.basePath + '/item/' + modItem.id)
+                                .set('Accept', 'application/json')
+                                .expect('Content-Type', /json/)
+                                .expect(200)
+                                .end(function(err, res) {
+                                    should.not.exist(err);
+                                    done();
+                                });
+                        }).catch(err => console.error(err));
                 });
             });
         });
