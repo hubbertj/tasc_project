e5f903c242143ebfc860de65206f2fd5c628c18b
Jerum Hubbert <hubbertj@gmail.com>
2018-08-01 02:52:47 -0700

    adding script for making a purchase

---
+++ tasc_exercise/scripts/tasc_test.js
@@ -0,0 +1,15 @@
+'use strict';
+/*  Title: Tasc Test
+    Author:  Jerum Hubbert
+    Date: Aug 01 2018
+    Comment: 
+        This script should print out a receipt for a define transaction in the readme.
+*/
+
+console.log("run the tast test file..");
+
+//get the date we need from the json database files.
+//create the server.
+//run a health test to make sure the server is up.
+//run a /checkout with the items we want.
+//create a receipt from the date we get back.
